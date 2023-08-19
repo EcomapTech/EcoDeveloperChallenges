14140e5350a51e82f4b6debe8078418c113416ec
Duncan <duncanwoodpro@gmail.com>
2023-08-18 20:17:56 -0400

    Curren tly figuring out gensim and word2vec to train a model to find the  nearest words that are searched. Currently it works, but works poorly. Tried to use NLTK as it seems like the best option but it doesnt work on my mac so I used spacy to train it. Will continue  to develop the API but I might switch over to the frontend since  it is easier for me right now.

---
+++ Backend/search-engine-flask/Word2Vec-Training/train_word2vec.py
@@ -0,0 +1,39 @@
+import gensim
+from gensim.models import Word2Vec
+from pathlib import Path
+import spacy
+from spacy.lang.en import English
+
+# Load the spaCy English model
+nlp = English()
+
+# Add the sentencizer component to the pipeline
+nlp.add_pipe("sentencizer")
+
+# Load the text from the file
+corpus_path = '../corpus/hemingway.txt'
+with open(corpus_path, 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# Preprocess the text
+def preprocess_text(text):
+    doc = nlp(text)
+    tokenized_corpus = [
+        [token.text.lower() for token in sent if not token.is_punct]
+        for sent in doc.sents
+    ]
+    return tokenized_corpus
+
+# Preprocess the text and train the Word2Vec model
+tokenized_corpus = preprocess_text(text)
+model = Word2Vec(
+    sentences=tokenized_corpus,
+    vector_size=100,    # Dimensionality of the word vectors
+    window=5,           # Maximum distance between the current and predicted word within a sentence
+    min_count=5,        # Ignores words with total frequency lower than this
+    sg=0,               # Training algorithm: 0 for CBOW, 1 for Skip-gram
+    workers=4           # Number of CPU cores to use for training
+)
+
+# Save the trained model for future use
+model.save('trained_word2vec_model.model')
---
+++ Backend/search-engine-flask/search-engine/index.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+from gensim.models import Word2Vec
+
+app = Flask(__name__)
+
+# Load the trained Word2Vec model
+model = Word2Vec.load('./Word2Vec-Training/trained_word2vec_model.model')
+
+# Define the Flask routes
+
+
+@app.route('/')
+def hello():
+    return """
+    <h1>Welcome to the Search Engine API</h1>
+    <p>This API provides the following endpoints:</p>
+    <ul>
+        <li><a href="/similar_words?query=word">/similar_words?query=word</a> - Get similar words to a query word</li>
+        <li><a href="/add_word" target="_blank">/add_word</a> - Add a new word to the search corpus</li>
+        <li><a href="/remove_similar_word?word=target_word">/remove_similar_word?word=target_word</a> - Remove the most similar word to a target word</li>
+    </ul>
+    """
+
+
+@app.route('/similar_words', methods=['GET'])
+def get_similar_words():
+    query = request.args.get('query')  # Get the query word from the request
+    similar_words = model.wv.most_similar(
+        query, topn=3)  # Get the most similar words
+    return jsonify(similar_words)
+
+
+@app.route('/add_word', methods=['POST'])
+def add_word():
+    new_word = request.json['word']  # Get the new word from the request
+    # Update the vocabulary with the new word
+    model.build_vocab([new_word], update=True)
+    return jsonify({'message': f'Word {new_word} added to the search corpus.'})
+
+
+@app.route('/remove_similar_word', methods=['DELETE'])
+def remove_similar_word():
+    # Get the target word from the request
+    target_word = request.args.get('word')
+    most_similar = model.wv.most_similar(target_word, topn=1)[
+        0][0]  # Get the most similar word
+    # Remove the word by zeroing its vector
+    model.wv.vectors[model.wv.vocab[most_similar].index] = 0
+    return jsonify({'message': f'Most similar word {most_similar} removed from search results.'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
