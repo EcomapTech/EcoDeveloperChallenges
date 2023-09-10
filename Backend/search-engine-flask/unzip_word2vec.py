d5234b52e0aa2a13d8572465513b2d48c41bbad5
Duncan <duncanwoodpro@gmail.com>
2023-09-10 12:21:22 -0400

    feeling good about the progress I made over the weekend with the similarity functions. I will now merge the branch and get it working with the frontend.

--- Backend/search-engine-flask/unzip_word2vec.py
+++ Backend/search-engine-flask/unzip_word2vec.py
@@ -1,9 +1,13 @@
+# This file is not used during the execution of the program.
+# It is only used to unzip the GoogleNews word2vec file.
+# I included it here for completeness.
+
 """ Unzips the GoogleNews word2vec file. """
 import gzip
 import shutil
 
-INPUT_FILE = '/Users/duncanwood/Desktop/GoogleNews-vectors-negative300.bin.gz'
-OUTPUT_FILE = '/Users/duncanwood/Desktop/GoogleNews-vectors-negative300.bin'
+INPUT_FILE = '/Users/myname/Desktop/GoogleNews-vectors-negative300.bin.gz'
+OUTPUT_FILE = '/Users/myname/Desktop/GoogleNews-vectors-negative300.bin'
 
 with gzip.open(INPUT_FILE, 'rb') as f_in:
     with open(OUTPUT_FILE, 'wb') as f_out:
--- Backend/search-engine-flask/word2vec_vocabulary.py
+++ Backend/search-engine-flask/word2vec_vocabulary.py
@@ -1,3 +1,8 @@
+# this file is not used during the execution of the program.
+# It is only used to create the vocabulary file.
+# I did this because the vocabulary file is too large to upload to GitHub.
+# I included it here for completeness.
+
 """ This script creates a set of words found in the Word2Vec model and saves it to a file. """
 import pickle
 from gensim.models import KeyedVectors
@@ -8,7 +13,7 @@ word_list = corpus_utils.load_corpus(config.CORPUS_FILE_PATH)
 
 # Load the pre-trained Word2Vec model
 word2vec_model = KeyedVectors.load_word2vec_format(
-    '/Users/duncanwood/Desktop/GoogleNews-vectors-negative300.bin', binary=True)
+    '/Users/myname/Desktop/GoogleNews-vectors-negative300.bin', binary=True)
 
 # Create a dictionary of words and their vectors found in the Word2Vec model
 model_vocabulary_filtered = {}
